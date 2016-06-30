f9301c9206ef842d600b0d78d1688279fe6b0e5e
Jacek Skwarna <jacek.skwarna@blstream.com>
2016-06-30 11:33:05 +0200

    Add speed module to resources.

--- resources/index.js
+++ resources/index.js
@@ -1,4 +1,5 @@
 //exports.categories = require('./categories');
 exports.gpio = require('./gpio');
 exports.switches = require('./switches');
+exports.speed = require('./speed');
 //exports.users = require('./users');
